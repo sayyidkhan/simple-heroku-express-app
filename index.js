e138fb8893e5f4bf4ffd7aa7042c592d421e392d
Sayyid Khan <sayyidkhan92@hotmail.com>
2018-11-05 10:54:59 +0800

    successfully installed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.port || 3000;
 
 app.get('/', function(req,res){
-res.send('Welcome to NodeJS App on Heroku!');
+res.send('Successfully installed, Welcome to NodeJS App on Heroku!');
 });
 
 app.listen(port);
